5259ddb5ca47da5bc8384dbbc688e69c3dc7124a
KasperDev <kasper.php@gmail.com>
2018-08-30 13:47:48 +0300

    start play FIX - asar archive

--- public/connect.js
+++ public/connect.js
@@ -1,20 +1,20 @@
-const { execFile, spawn } = require('child_process');
+const { execFile, spawn, exec } = require('child_process');
 const path = require('path');
 
-let pidvpn = null;
-let playpid = null;
+let OVPPID = null;
+let PIDPLAY = null;
 
 const cert = `${__dirname}/win10/ovp/CERTIFICATE.ovpn`;
 const ovpshel = `${__dirname}/win10/ovp/openvpn.exe`;
-const play = `${__dirname}/win10/play/Remote Play PS4.exe`;
-console.log(ovpshel);
+const play = `${__dirname}\\win10\\play\\`;//RemotePlayPS4.exe
+
 exports.Connect = function(win) {
   const Process = execFile(ovpshel, ['--config', cert], (error, stdout, stderr) => {
     Process.pid.kill();
     win.show();
     console.log("Open VPN CONNECT Close");
   });
-  pidvpn = Process;
+  OVPPID = Process;
 
   console.log(`Spawned child pid: ${Process.pid}`);
 
@@ -27,21 +27,23 @@ exports.Connect = function(win) {
     if (data.split('Sequence ')[1] !== undefined) {
        console.log('Initialization Connekt: OK');
          win.hide();
-          const Strim = spawn(play);
-          playpid = Strim.pid;
-
-        console.log(`Playpid child pid: ${playpid}`);
+    let PIDPLAY = exec(`cd ${play} && RemotePlayPS4.exe`, (err, stdout, stderr) => {
+              if (err) {
+                console.error(err);
+                return;
+              }
+              console.log(stdout);
+             });
+          PIDPLAY = PIDPLAY.pid;
+        console.log(`Playpid child pid: ${PIDPLAY}`);
     }
-        console.log(data);
   });
 }
 
 exports.Disconnect = function () {
-    if (pidvpn === null){
+    if (OVPPID === null){
       return console.log("No connection");
     }
-    pidvpn.kill();
-
-
-  console.log("Disconnect Succes");
+    OVPPID.kill();
+ console.log("Disconnect Succes");
 }
